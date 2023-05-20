8f28f2201913c857047985f010e0e2e50a4c6379
Damian <bzdyrad@gmail.com>
2023-05-20 16:49:53 -0400

    add getAllHealth route

--- controllers/health.js
+++ controllers/health.js
@@ -1,6 +1,21 @@
 import Profile from "../models/profile.js";
 import Health from "../models/health.js";
 
+async function getAllHealth(req, res) {
+  try {
+    const { category } = req.query;
+
+    const health = !category
+      ? await Health.find({})
+      : await Health.find({ category });
+
+    res.status(200).json(health);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json(error);
+  }
+}
+
 async function createHealth(req, res) {
   try {
     const profileId = req.token.user.profile;
@@ -27,4 +42,4 @@ async function deleteHealth(req, res) {
   }
 }
 
-export { createHealth, deleteHealth };
+export { getAllHealth, createHealth, deleteHealth };
--- routes/health.js
+++ routes/health.js
@@ -4,6 +4,7 @@ import * as healthController from "../controllers/health.js";
 
 const router = Router();
 
+router.get("/", healthController.getAllHealth);
 router.post("/", auth, healthController.createHealth);
 
 router.delete("/:healthId", auth, healthController.deleteHealth);
